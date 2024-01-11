2ea358f56294bd7f18c9d9bb45dcf4c824452579
Rahul Kumar <rahulkumarair@Rahuls-Laptop.local>
2024-01-11 14:03:30 +0530

    maximum_depth_binary_tree

---
+++ maximum_depth_of_binary_tree.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
